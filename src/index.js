19851860b99fae9111bbaba482589b436ef18f0c
Moyboy <Vincent.moy94@gmail.com>
2024-08-12 01:11:10 -0500

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   document.querySelector('form').addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     const task = document.querySelector('#new-task-description').value
+//     const listItem = document.createElement('li')
+//     listItem.textContent = task
+//     document.querySelector('#tasks').appendChild(listItem)
+//     console.log(listItem)
+//     e.target.reset()
+//   })
+
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(document.querySelector('#new-task-description').value)
+    form.reset()    
+  })
+
 });
+
+function buildToDo(todo){
+    const task = document.querySelector('#new-task-description').value
+    const listItem = document.createElement('li')
+    const btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'X'
+    listItem.textContent = `${todo}`
+    listItem.appendChild(btn)
+    document.querySelector('#tasks').appendChild(listItem)
+    console.log(listItem)
+}
+
+function handleDelete(listItem){
+    listItem.target.parentNode.remove()
+}
